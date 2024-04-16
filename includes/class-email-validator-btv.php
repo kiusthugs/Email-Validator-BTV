44737d7f06183cd883547f9cbdeaba82b6e1cf43
Kirt Perez <kirtperez3245@gmail.com>
2024-04-16 15:15:24 -0700

    Updated code placement, removed 2 instances of plugins

--- includes/class-email-validator-btv.php
+++ includes/class-email-validator-btv.php
@@ -78,6 +78,7 @@ class Email_Validator_Btv {
 		$this->set_locale();
 		$this->define_admin_hooks();
 		$this->define_public_hooks();
+		$this->validate();
 
 	}
 
@@ -213,6 +214,45 @@ class Email_Validator_Btv {
 	 */
 	public function get_version() {
 		return $this->version;
+	}
+
+	public function validate() {
+		if(class_exists('GFAPI')) {
+			 add_filter( 'gform_field_validation', function ( $result, $value, $form, $field ) {
+				if ( $field->get_input_type() === 'email' && $result['is_valid'] ) {
+			
+					$email = $value;
+					$key = 'MyXfsOi1WGzLARyZoMjzB';
+					$query_args = array(
+						'secret' => $key,
+						'email' => $email,
+						'timeout' => 15
+					);
+			
+					$request_url = add_query_arg( $query_args, 'https://apps.emaillistverify.com/api/verifyEmail' );
+					echo $request_url;
+			
+					// True to submit, False to decline
+					//Code 200 returns ok = true, Code 401 returns email_disabled = false
+			
+					$ch = curl_init();
+					curl_setopt($ch, CURLOPT_URL, $request_url);
+					curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+					curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true );
+			
+					$response = curl_exec($ch);
+					echo $response;
+					curl_close($ch);
+			
+					if ($response !== 'ok') {
+						$result['is_valid'] = false;
+					}
+				}
+			  
+				return $result;
+			}, 10, 4 );
+			}
 	}
 
 }
